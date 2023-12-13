95563e4884ad792959db0b6bd98b9743b664d87b
Boulbir Khalil <k_boulbir@enst.dz>
2023-12-13 04:42:41 -0800

    setup home page , postgresql configuration and .ven-sample

--- onligneresturant/settings.py
+++ onligneresturant/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+_74w^12pj4j)jbb73@vsx@x1g_!ejrwp@_9)x9x)_5qjd64=7'
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'onligneresturant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +74,11 @@ WSGI_APPLICATION = 'onligneresturant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        "USER":config("DB_USER"),
+        "PASSWORD":config("DB_PASSWORD"),
+        "HOST":config("DB_HOST"),
     }
 }
 
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=BASE_DIR /'static'
+STATICFILES_DIRS=[
+    "onligneresturant/static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
